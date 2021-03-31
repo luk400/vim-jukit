1164fb4a9d26267a66b8bc8d5512b2d100d19674
lukas <pichlmannlukas@gmail.com>
2021-03-31 23:33:06 +0200

    simplified script

--- helpers/check_matplotlib_backend.py
+++ helpers/check_matplotlib_backend.py
@@ -2,37 +2,16 @@ import site
 import os
 import sys
 
-def get_path():
-    all_paths = site.getsitepackages()
-    path = None
-    while (not path and len(all_paths)>1):
-        print("\n".join(f"{i}: " + el for i, el in enumerate(all_paths)))
-        idx = input(("Multiple Paths found, please choose one of the above by the given " 
-                "number to import (or create if non-existent) the backend module:\n"))
-        try:
-            idx = int(idx)
-        except ValueError:
-            print("Choose a valid path by specifying the given index!")
-        if (type(idx)!=int) and ((idx < 0) or (idx >= len(all_paths))):
-            print("Choose a valid path by specifying the given index!")
-        else:
-            path = all_paths[idx]
-    
-    if len(all_paths)==1:
-        path = all_paths[0]
-    
-    return path
-
 if __name__=="__main__":
-    import os
-    import sys
     plugin_path = str(sys.argv[1])
-    
     backend_dir = plugin_path + '/helpers/matplotlib-backend-kitty'
-    path = get_path() + '/'
-    if not "matplotlib-backend-kitty" in os.listdir(path):
-        os.system(f"cp -r {backend_dir} {path}")
-        print(f"\nCreated matplotlib-backend-kitty module in {path}\n")
+
+    paths = [path + '/' for path in site.getsitepackages()]
+    all_modules = [os.listdir(path) for path in paths]
+    all_modules = [el for sublist in all_modules for el in sublist] # flatten
+    if not "matplotlib-backend-kitty" in all_modules:
+        os.system(f"cp -r {backend_dir} {paths[0]}")
+        print(f"\nCreated matplotlib-backend-kitty module in {paths[0]}\n")
     else:
-        print(f"\nUsing matplotlib-backend-kitty module found in {path}\n")
+        print(f"\nUsing matplotlib-backend-kitty module found in {paths}\n")
 
