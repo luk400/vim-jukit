0bf0ca9075874012c6ba3150d43fe561557876fa
luk400 <pichlmannlukas@gmail.com>
2023-04-02 20:08:11 +0200

    update multiline comments for matlab/octave

--- helpers/ipynb_convert/languages.py
+++ helpers/ipynb_convert/languages.py
@@ -3,7 +3,7 @@ languages = {
     "r": {"ext": "R", "cchar": "#", "multiline_start": '"', "multiline_end": '"', "kernel_name": "R"},
     "julia": {"ext": "jl", "cchar": "#", "multiline_start": '#=', "multiline_end": '=#', "kernel_name": "julia"},
     "java": {"ext": "java", "cchar": "//", "multiline_start": '/*', "multiline_end": '*/', "kernel_name": "java"},
-    "matlab": {"ext": "m", "cchar": "%", "multiline_start": '%{', "multiline_end": '%}', "kernel_name": "matlab"},
+    "matlab": {"ext": "m", "cchar": "%", "multiline_start": '%{\n', "multiline_end": '\n%}', "kernel_name": "matlab"},
     "rust": {"ext": "rs", "cchar": "//", "multiline_start": '/*', "multiline_end": '*/', "kernel_name": "rust"},
     "lua": {"ext": "lua", "cchar": "--", "multiline_start": '--[[', "multiline_end": '--]]', "kernel_name": "lua"},
 }
